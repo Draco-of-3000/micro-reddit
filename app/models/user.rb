824ba271eba7f3d49295ae1ba1a378501d4d7e51
Draco <kevin.tenisson@gmail.com>
2025-03-31 09:58:41 -0700

    updated user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  
   validates :username, presence: true, uniqueness: true, length: { in: 4..12 }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, length: { in: 8..16 }
