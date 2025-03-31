befe59a0ab3007a90bf593c0c12b145cccc0c85f
Draco <kevin.tenisson@gmail.com>
2025-03-31 09:56:09 -0700

    validation for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]) }
 end
